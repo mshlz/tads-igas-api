f4c23cee93b77374ff69708c2f20cc9d035e6387
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2022-04-10 17:08:47 -0300

    add delete product

--- src/controllers/DistributerProductController.ts
+++ src/controllers/DistributerProductController.ts
@@ -1,9 +1,10 @@
-import { Body, Get, JsonController, Param, Post, Put, UseBefore } from 'routing-controllers'
+import { Body, Delete, Get, JsonController, Param, Post, Put, UseBefore } from 'routing-controllers'
 import { CreateProductInput } from '../dto/product/CreateProductInput'
 import { UpdateProductInput } from '../dto/product/UpdateProductInput'
 import { SessionUser } from '../helpers/decorators/SessionUser'
 import { success } from '../helpers/http/responses'
 import { IsDistributer } from '../middlewares/IsDistributer'
+import { getPublicProduct } from '../models/Product'
 import { UserModel } from '../models/User'
 import ProductService from '../services/ProductService'
 
@@ -22,14 +23,14 @@ export class DistributerProductController {
         const { name, description, image, price, quantity } = body
         const product = await ProductService.create(name, description, image, price, quantity, user._id)
 
-        return success(product)
+        return success(getPublicProduct(product))
     }
 
     @Get('/:id')
     async getProduct(@Param('id') productId: string, @SessionUser() user: UserModel) {
         const product = await ProductService.getOne(productId, user._id)
 
-        return success(product)
+        return success(getPublicProduct(product))
     }
 
     @Put('/:id')
@@ -37,7 +38,13 @@ export class DistributerProductController {
         const { name, description, image, price, quantity } = body
         const product = await ProductService.update(productId, user._id, name, description, image, price, quantity)
 
-        return success(product)
+        return success(getPublicProduct(product))
+    }
+    
+    @Delete('/:id')
+    async deleteProduct(@Param('id') productId: string, @SessionUser() user: UserModel) {
+        await ProductService.deleteOne(productId, user._id)
+        return success(true)
     }
 
 }
--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,5 +1,5 @@
 import { NotFoundError, UnauthorizedError } from "routing-controllers"
-import { getPublicProduct, Product, ProductModel } from "../models/Product"
+import { getPublicProduct, Product } from "../models/Product"
 
 class ProductService {
     async listAllFromDistributer(distributerId: string) {
@@ -20,31 +20,9 @@ class ProductService {
             owner: distributerId
         })
 
-        return getPublicProduct(product)
+        return product
     }
 
-    async update(productId: string, distributerId: string, name: string, description: string, image: string, price: number, quantity: number) {
-        const product = await Product.findById(productId)
-
-        if (!product) {
-            throw new NotFoundError('Product not found')
-        }
-
-        if (String(product.owner) !== String(distributerId)) {
-            throw new UnauthorizedError('This product does not belong to you')
-        }
-
-        product.name = name
-        product.description = description
-        product.image = image
-        product.price = price
-        product.quantity = quantity
-
-        await product.save()
-
-        return getPublicProduct(product)
-    }
-    
     async getOne(productId: string, distributerId: string) {
         const product = await Product.findById(productId)
 
@@ -56,7 +34,28 @@ class ProductService {
             throw new UnauthorizedError('This product does not belong to you')
         }
 
-        return getPublicProduct(product)
+        return product
+    }
+
+    async update(productId: string, distributerId: string, name: string, description: string, image: string, price: number, quantity: number) {
+        const product = await this.getOne(productId, distributerId)
+
+        product.name = name
+        product.description = description
+        product.image = image
+        product.price = price
+        product.quantity = quantity
+
+        await product.save()
+
+        return product
+    }
+
+    async deleteOne(productId: string, distributerId: string) {
+        const product = await this.getOne(productId, distributerId)
+        await product.delete()
+
+        return true
     }
 
 }
